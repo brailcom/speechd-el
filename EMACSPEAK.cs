1fba1823283018d893f372a2b750153dd0379539
pdm <pdm>
2003-09-16 12:35:01 +0000

    Initial revision.

---
+++ EMACSPEAK.cs
@@ -0,0 +1,42 @@
+Jaké jsou rozdíly mezi speechd-el a Emacspeak?
+
+Obecnì vzato, speechd-el a Emacspeak jsou nezávislé a odli¹ným zpùsobem
+navr¾ené nástroje slou¾ící podobnému úèelu.  Pøedpokládáme, ¾e budou pou¾ívány
+rùznými skupinami u¾ivatelù, s rozdílnými potøebami.
+
+Hlavní zásadní rozdíl mezi speechd-el a Emacspeak je v tom, ¾e speechd-el se
+sna¾í být ve v¹ech smìrech jednoduché a spoléhající na slu¾by Speech
+Dispatcher, zatímco Emacspeak se sna¾í být v¹estranný a komplexní a øe¹it v¹e
+sám.
+
+Konkrétní dùvody, proè mù¾ete chtít pou¾ívat speechd-el:
+
+- Chcete pou¾ívat Speech Dispatcher.  Emacspeak jej momentálnì nepodporuje a je
+  nepravdìpodobné, ¾e by jej v budoucnu podporoval více ne¾ omezenì, vzhledem
+  k architektuøe Emacspeak.
+
+- Jste zvyklí na standardní emacsové prostøedí a rádi byste pou¾ívali systém,
+  který v nìm nemìní více ne¾ je nezbytnì nutné.
+
+- Dáváte pøednost vlastnoruèním nastavením, s pøípadným vyu¾itím poskytnutých
+  konfiguraèních nástrojù, pøed prostøedím pøedem zkonfigurovaným, ne nutnì vám
+  vyhovujícím zpùsobem.
+
+- Chcete provádìt zmìny nebo pøizpùsobení v emacsovém øeèovém systému samotném
+  a dávate v tomto smìru pøednost malým a èistì napsaným programùm.
+
+Konkrétní dùvody, proè mù¾ete chtít pou¾ívat Emacspeak:
+
+- Emacspeak ji¾ pou¾íváte a jste s ním naprosto spokojeni.
+
+- Nechcete pou¾ívat Speech Dispatcher.
+
+- Chcete mít v¹e pøedem zkonfigurováno a chcete mít zmìnìno standardní chování
+  Emacsu zpùsobem, který se pokou¹í nabídnout speciální prostøedí zrakovì
+  posti¾eným u¾ivatelùm.
+
+- Upøednostòujete øe¹ení, která jsou pou¾ívána a vyvíjena ji¾ øadu let.
+
+®ádný z emacsových øeèových systémù nedisponuje nìjakou pøevratnou funkcí,
+která by jej èinila nenahraditelným, tak¾e zále¾í èistì na va¹em rozhodnutí a
+va¹ich potøebách, který z nich si vyberete.
