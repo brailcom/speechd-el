e2cb61386a80e2fa2ce399faf53ac64f7ba6373d
pdm <pdm>
2004-09-08 10:44:11 +0000

    speechd-version.el

--- speechd-version.el
+++ speechd-version.el
@@ -1,2 +1,2 @@
-(defconst speechd-version " pdm@brailcom.org--pdm/speechd-el--main--0.5--patch-39 ")
+(defconst speechd-version " pdm@brailcom.org--pdm/speechd-el--main--0.5--patch-43 ")
 (provide 'speechd-version)
