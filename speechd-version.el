f9a38a2c553d95f880513b6141a3093f679b1da3
pdm <pdm>
2004-10-18 20:32:30 +0000

    Version stamp update

--- speechd-version.el
+++ speechd-version.el
@@ -1,2 +1,2 @@
-(defconst speechd-version " pdm@brailcom.org--pdm/speechd-el--main--0.5--patch-71 ")
+(defconst speechd-version " pdm@brailcom.org--pdm/speechd-el--main--0.5--patch-72 ")
 (provide 'speechd-version)
