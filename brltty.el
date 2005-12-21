1e2070187d6587a95b29c3a753944f7c3e6eaeb7
pdm <pdm>
2005-12-21 22:08:30 +0000

    Wed Dec 21 13:03:10 CET 2005  pdm@brailcom.org   * Reading terminal number from /proc/self/stat implemented

--- brltty.el
+++ brltty.el
@@ -132,9 +132,17 @@ available, from the  environment variable CONTROLVT."
      ((getenv "WINDOWID")
       (push (string-to-number (getenv "WINDOWID")) terminal-spec))
      ((file-readable-p "/proc/self/stat")
-      ;; where's the tty info in `stat'?
-      (push 0 terminal-spec)
-      )
+      (with-temp-buffer
+        (push (or (ignore-errors
+                    (insert-file-contents "/proc/self/stat")
+                    (goto-char (point-min))
+                    (dotimes (_ 6) (read))
+                    (let ((tty (read)))
+                      (if (and tty (= (/ tty 256) 4)) ; major
+                          (mod tty 256)               ; minor
+                        0)))
+                  0)
+              terminal-spec)))
      (t
       (push 0 terminal-spec)))
     (nreverse terminal-spec)))
