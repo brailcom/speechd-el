a4be22b5b62a6be1e749df6f64b06e16a6b09790
Milan Zamazal <pdm@zamazal.org>
2021-12-31 08:58:43 +0100

    Add speechd-el.el
    straight.el looks up for this file and issues a warning if the file is
    not present.  Let’s avoid the warning and make straight.el happy.

---
+++ speechd-el.el
@@ -0,0 +1,24 @@
+;;; speechd-el.el --- dummy file for straight.el  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Milan Zamazal
+
+;; Author: Milan Zamazal <pdm@zamazal.org>
+
+;; COPYRIGHT NOTICE
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(provide 'speechd-el)
+
+;;; speechd-el.el ends here
