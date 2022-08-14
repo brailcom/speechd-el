3d6a331323634585ef0eace86d2b8872c39059af
Milan Zamazal <pdm@zamazal.org>
2022-08-14 19:34:18 +0200

    Add more information to speechd-el.el

--- speechd-el.el
+++ speechd-el.el
@@ -1,8 +1,9 @@
-;;; speechd-el.el --- dummy file for straight.el  -*- lexical-binding: t -*-
+;;; speechd-el.el --- info file for package managers  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021 Milan Zamazal
+;; Copyright (C) 2021-2022 Milan Zamazal
 
 ;; Author: Milan Zamazal <pdm@zamazal.org>
+;; URL: https://github.com/brailcom/speechd-el
 
 ;; COPYRIGHT NOTICE
 ;;
@@ -18,6 +19,22 @@
 ;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; speechd-el is an Emacs client to speech synthesizers, Braille
+;; displays and other alternative output interfaces.  It provides full
+;; speech and Braille output environment for Emacs.
+
+;; It is aimed primarily at visually impaired users who need
+;; non-visual communication with Emacs, but it can be used by anybody
+;; who needs sophisticated speech or other kind of alternative output
+;; from Emacs.  speechd-el can make Emacs a completely speech and
+;; BRLTTY enabled application suitable for visually impaired users or,
+;; depending on its configuration, it can only speak in certain
+;; situations or when asked, to serve needs of any Emacs user.
+
+;;; Code:
 
 (provide 'speechd-el)
 
